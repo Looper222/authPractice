8f577dce4cd1fcceb85c11c134ee52f9a099e528
Dawid <dawid.jm@interia.pl>
2022-03-21 23:34:13 +0100

    Updated services of users and auth + added User model

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { User } from 'src/users/models/User';
 import { UsersService } from '../users/users.service';
+
 
 @Injectable()
 export class AuthService {
@@ -8,4 +10,8 @@ export class AuthService {
         private readonly usersService: UsersService,
         private readonly JwtService: JwtService
         ) {}
+
+        async validate(login: string, password: string): User | null {
+            const user = this.usersService.getUser(login);
+        }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './models/User';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    private users: User[] = [];
+
+    public getUser(getUserArgs: GetUserArgs): User {
+        return this.users.find(user => user.id === getUserArgs.userId);
+    }
+}
